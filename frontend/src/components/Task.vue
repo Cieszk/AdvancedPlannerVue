43e7bf59de81fc8881d95a51b33001d1b86507ba
Kamil Cieszkowski <cieszkens@gmail.com>
2020-06-03 21:15:24 +0200

    adding 'archive task' feature

---
+++ frontend/src/components/Task.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <button class="btn btn-sm btn-secondary ml-2" @click="triggerArchiveTask">Done</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Task",
+        props: {
+            task: {
+                type: Object,
+                required: true
+            }
+        },
+        methods: {
+            triggerArchiveTask() {
+                this.$emit('archive-task', this.task)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/views/Todolist.vue
+++ frontend/src/views/Todolist.vue
@@ -1,24 +1,43 @@
 <template>
-    <div class="container mt-5">
-        <div class="task-border mb-3" v-for="task in tasks"
-             :key="task.pk">
-            <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1">{{ task.name }}</h5>
-                <small class="text-muted">{{ task.created_at }}<button type="button" class="btn btn-sm btn-secondary ml-2">Done</button></small>
+    <div>
+        <div class="container mt-5" v-if="tasks.done">
+            <div class="task-border mb-3" v-for="task in tasks"
+                 :key="task.id">
+                <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1">{{ task.name }}</h5>
+                    <div style="display: inline-block;">
+                        <small class="text-muted">{{ task.created_at }}
+
+                        </small>
+                        <Task
+                                :key="task.id"
+                                :task="task"
+                                @archive-task="archiveTask"
+                        />
+                    </div>
+                </div>
+                <small class="text-muted">{{ task.description }}</small>
             </div>
-            <small class="text-muted">{{ task.description}}</small>
+        </div>
+        <div>
+            <div class="form-group mt-4">
+                <label for="exampleTextarea">Add new task</label>
+                <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
     import {apiService} from "../common/api.service";
+    import Task from "../components/Task";
 
     export default {
         name: "Todolist",
+        components: {Task},
         data() {
             return {
-                tasks: []
+                tasks: [],
             }
         },
         methods: {
@@ -28,6 +47,17 @@
                     .then(data => {
                         this.tasks.push(...data.results);
                     })
+            },
+            async archiveTask(task) {
+                let endpoint = `/api/tasks/${task.id}`;
+                var task_index;
+                try {
+                    await apiService(endpoint);
+                    task_index = this.tasks.indexOf(task);
+                    Object.assign(this.tasks[task_index], {done: true})
+                } catch (err) {
+                    console.log(err)
+                }
             }
         },
         created() {
